3ac7596c3920d70c08c18f1f3d5731c543837046
Louis-Philippe Huberdeau <lp@huberdeau.info>
2015-11-12 17:08:08 -0500

    Apply timeout when checking ports

--- dab.py
+++ dab.py
@@ -11,6 +11,9 @@ import subprocess
 from nmb import NetBIOS
 from subprocess import check_output
 
+
+
+
 # SSH PORTS
 SSH_PORTS = [22]
 
@@ -36,6 +39,19 @@ if len(sys.argv) <= 1:
     sys.exit()
 
 
+def is_open(port):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(0.5)
+        s.connect((args.host, port))
+        s.close()
+        return True
+    except:
+        return False
+
+def list_open(ports):
+    return [p for p in ports if is_open(p)]
+
 # Hash Confidence level:i
 # 1- TLS host fingerprint (preferred)
 # 2- Hostnamei hash
@@ -49,60 +65,33 @@ except:
 
 results['hostname'] = hostname
 
-for port in SSH_PORTS:
-    connected = True
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((args.host, port))
-        s.close()
-    except:
-        connected = False
-
-    if connected:
-        keyscan_out = check_output(['ssh-keyscan', args.host], stderr=subprocess.PIPE).decode('utf-8')
-        # Strip ending \n
-        keyscan_out = keyscan_out.strip('\n')
-        entries = keyscan_out.split('\n')
-        for entry in entries:
-            ip, htype, b64hash = entry.split(' ')
-            results['ssh_fingerprints'].append({'type': htype, 'hash': b64hash})
+for port in list_open(SSH_PORTS):
+    keyscan_out = check_output(['ssh-keyscan', args.host], stderr=subprocess.PIPE).decode('utf-8')
+    # Strip ending \n
+    keyscan_out = keyscan_out.strip('\n')
+    entries = keyscan_out.split('\n')
+    for entry in entries:
+        ip, htype, b64hash = entry.split(' ')
+        results['ssh_fingerprints'].append({'type': htype, 'hash': b64hash})
 
 
-for port in TLS_PORTS:
-    connected = True
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((args.host, port))
-        s.close()
-    except:
-        connected = False
+for port in list_open(TLS_PORTS):
+    # Try to get a TLS fingerprint then exit 
+    process = subprocess.Popen(['openssl', 's_client', '-connect', args.host + ':' + str(port)],stdout=subprocess.PIPE,stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+    process.stdin.write(b'Q\n')
+    raw_cert = process.communicate()[0]
+    process = subprocess.Popen(['openssl', 'x509', '-fingerprint', '-noout', '-in', '/dev/stdin'],stdout=subprocess.PIPE,stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+    process.stdin.write(raw_cert)
+    fingerprint = process.communicate()[0].decode('utf-8')
+    if fingerprint:
+        fingerprint = fingerprint.strip('\n')
+        results['ssl_fingerprints'].append(fingerprint.split('=')[1])
 
-    if connected:
-        # Try to get a TLS fingerprint then exit 
-        process = subprocess.Popen(['openssl', 's_client', '-connect', args.host + ':' + str(port)],stdout=subprocess.PIPE,stdin=subprocess.PIPE, stderr=subprocess.PIPE)
-        process.stdin.write(b'Q\n')
-        raw_cert = process.communicate()[0]
-        process = subprocess.Popen(['openssl', 'x509', '-fingerprint', '-noout', '-in', '/dev/stdin'],stdout=subprocess.PIPE,stdin=subprocess.PIPE, stderr=subprocess.PIPE)
-        process.stdin.write(raw_cert)
-        fingerprint = process.communicate()[0].decode('utf-8')
-        if fingerprint:
-            fingerprint = fingerprint.strip('\n')
-            print(repr(fingerprint))
-            results['ssl_fingerprints'].append(fingerprint.split('=')[1])
 
-for port in NBT_PORTS:
-    connected = True
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((args.host, port))
-        s.close()
-    except:
-        connected = False
-
-    if connected:
-        client = NetBIOS.NetBIOS()
-        nbt_name = client.queryIPForName(args.host, timeout=5)
-        if len(nbt_name) > 0:
-            results['nbt_hostname'] = nbt_name[0]
+for port in list_open(NBT_PORTS):
+    client = NetBIOS.NetBIOS()
+    nbt_name = client.queryIPForName(args.host, timeout=5)
+    if len(nbt_name) > 0:
+        results['nbt_hostname'] = nbt_name[0]
 
 print(repr(results))
