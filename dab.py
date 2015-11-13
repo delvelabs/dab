d98119a2fe3de2a6d339ee5aec2b12ee97d10ceb
Louis-Philippe Huberdeau <lp@huberdeau.info>
2015-11-13 13:26:40 -0500

    Adding copyright, license file

--- dab.py
+++ dab.py
@@ -1,6 +1,28 @@
 #!/usr/bin/env python3
-# Dab, a host fingerprint generator
-# Delve Labs inc. 2015.
+"""
+Dab, a host fingerprint generator
+
+Copyright (C) 2015-     Delve Labs inc. <info@delvelabs.ca>
+
+This software is provided 'as-is', without any express or implied warranty.
+In no event will the author be held liable for any damages arising from the
+use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+
+1. The origin of this software must not be misrepresented; you must not
+   claim that you wrote the original software. If you use this software
+   in a product, an acknowledgment in the product documentation would be
+   appreciated but is not required.
+
+2. Altered source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+
+3. This notice cannot be removed or altered from any source distribution.
+"""
+
 
 import asyncio
 import argparse
@@ -8,14 +30,6 @@ import sys
 
 from subprocess import check_output
 from dab import Dab
-
-# Hash Confidence level:
-# 1- TLS host fingerprint (preferred)
-# 2- Hostname hash
-# 3- All port checksum
-
-# Get hostname
-
 
 
 if __name__ == "__main__":
